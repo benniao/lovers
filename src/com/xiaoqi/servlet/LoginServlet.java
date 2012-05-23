490c5063f650b7b69f4c6e9e502143b414c7e280
liuda <liu.da101@gmail.com>
2012-05-23 22:31:51 +0800

    login

--- src/com/xiaoqi/servlet/LoginServlet.java
+++ src/com/xiaoqi/servlet/LoginServlet.java
@@ -1,5 +1,32 @@
 package com.xiaoqi.servlet;
 
-public class LoginServlet {
-	
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		if (username != null
+				&& password != null
+				&& (username.equals("xiaoxiao") && password.equals("xiaoxiao") || username
+						.equals("qiqi") && password.equals("qiqi"))) {
+			HttpSession session = req.getSession();
+			session.setAttribute("username", username);
+			resp.sendRedirect("/home.jsp");
+		} else {
+			resp.sendRedirect("/index.html");
+		}
+	}
+
 }
