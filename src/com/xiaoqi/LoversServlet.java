cfe61b275ade177959c574dd0fa38a08bcfd77ab
liuda <liu.da101@gmail.com>
2012-05-23 20:02:41 +0800

    test

--- src/com/xiaoqi/LoversServlet.java
+++ src/com/xiaoqi/LoversServlet.java
@@ -8,6 +8,7 @@ public class LoversServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
+		System.out.println("Test");
 		resp.getWriter().println("Hello, world");
 	}
 }
